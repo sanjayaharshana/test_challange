4ce7829b2c68439e5bbb4a1ae09991f0adb9e41c
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-06-28 14:44:06 +0530

    API Response Table Migration Create

---
+++ database/migrations/2022_06_28_091024_create_api_responses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api_responses', function (Blueprint $table) {
+            $table->id();
+            $table->text('endpoint_url');
+            $table->text('response_data')->nullable();
+            $table->text('response_time')->nullable();
+            $table->text('status_code')->nullable();
+            $table->text('start_time')->nullable();
+            $table->text('end_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('api_responses');
+    }
+};

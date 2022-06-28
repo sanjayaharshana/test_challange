76516191c230723b6552e4b6175b9e9ab37a4fd4
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-06-28 19:34:27 +0530

    Login/Register API Created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class,'get_method_invalid'])->name('get.register');
+Route::get('/login', [AuthController::class,'get_method_invalid'])->name('get.login');
+
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

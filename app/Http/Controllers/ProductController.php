c54c68157799614f627f49a8e86c31357c1fe2be
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-06-28 20:05:13 +0530

    Product Controller Get Data Funciton

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $product = Product::all();
+
+        return response()->json($product);
+    }
 }
